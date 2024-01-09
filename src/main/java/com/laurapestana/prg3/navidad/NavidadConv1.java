7cceb7a95142f0d07d215d6458a60e18ad6f45e5
laura-2022 <100377888+laura-2022@users.noreply.github.com>
2024-01-09 12:58:24 +0100

    Add files via upload

---
+++ src/main/java/com/laurapestana/prg3/navidad/NavidadConv1.java
@@ -0,0 +1,29 @@
+package com.laurapestana.prg3.navidad;
+
+import java.util.Scanner;
+
+public class NavidadConv1 {
+    private static Scanner scanner = new Scanner(System.in);
+    public NavidadConv1(){
+
+        // Solicitar al usuario que ingrese un número entero
+        System.out.print("Ingrese un número entero: ");
+        int numeroEntero = scanner.nextInt();
+
+        // Calcular el doble del número como un número real utilizando una función
+        double resultado = calcularDobleComoReal(numeroEntero);
+
+        // Imprimir el resultado
+        System.out.println("El doble del número como un número real es: " + resultado);
+
+        // Cerrar el scanner
+        scanner.close();
+    }
+
+    // Función para calcular el doble del número como un número real
+    public static double calcularDobleComoReal(int numero) {
+        return 2 * (double) numero;
+    }
+}
+
+
